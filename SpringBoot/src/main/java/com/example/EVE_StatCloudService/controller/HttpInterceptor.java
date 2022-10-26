c68d83f56d201050102f52e4f9c6adaff4b11c52
Hanwei Tang <tanghanwei0210@gmail.com>
2022-10-26 10:36:17 -0400

    update

---
+++ SpringBoot/src/main/java/com/example/EVE_StatCloudService/controller/HttpInterceptor.java
@@ -0,0 +1,68 @@
+package com.example.EVE_StatCloudService.controller;
+
+import com.example.fileloader.misc.StreamUtils;
+import com.example.fileloader.dao.FileEntryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+
+@Component
+public class HttpInterceptor implements HandlerInterceptor {
+
+    public static final String resourcePath = "./res";
+
+    @Autowired
+    private ServletContext servletContext;
+
+    public HttpInterceptor() {
+    }
+
+    public MediaType getMediaType(String fileName) {
+        try {
+            String mimeType = servletContext.getMimeType(fileName);
+            MediaType mediaType = MediaType.parseMediaType(mimeType);
+            return mediaType;
+        } catch (Exception e) {
+            return MediaType.APPLICATION_OCTET_STREAM;
+        }
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
+
+
+        if ("".equals(uri) || "/".equals(uri) || "/home.html".equals(uri)) {
+            if (true) {
+                uri = "/home.html";
+            }
+            else {
+                uri = "/login.html";
+            }
+        }
+
+
+        return true;
+    }
+
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception exception) throws Exception {
+    }
+
+}
+
